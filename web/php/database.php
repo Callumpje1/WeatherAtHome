06007fc5b1a2618990b1645448c1f51ba3212197
Callum Svadkovski <callum.svadkovski@hva.nl>
2023-09-28 11:58:37 +0200

    Web
    Connection established with sql database

--- web/php/database.php
+++ web/php/database.php
@@ -1,25 +1,25 @@
 <?php
-$servername = "3306:3306";
-$username = "root";
+$host = "127.0.0.1";
+$port = 3307;
+$socket = "";
+$user = "root";
 $password = "Homerus4a!";
-$dbname = "testdb";
+$dbname = "forcecast";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+$conn = new mysqli($host, $user, $password, $dbname, $port, $socket)
+  or die('Could not connect to the database server' . mysqli_connect_error());
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT weatherId, temperature, time FROM weather";
+$sql = "SELECT * FROM Weather";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
-    echo "weatherId: " . $row["weatherId"]. " - temperature: " . $row["temperature"]. " " . $row["time"]. "<br>";
+  while ($row = $result->fetch_assoc()) {
+    echo json_encode($row);
   }
 } else {
   echo "0 results";
 }
 $conn->close();
-?>
--- web/php/testdata.php
+++
@@ -1,7 +0,0 @@
-<?php
-$data = array("temperature" => "21 C*", "location" => "Paris", "conditions" => "Chance of rain");
-
-header('Content-Type: application/json');
-
-echo json_encode($data);
-?>
