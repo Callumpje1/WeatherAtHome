305882d94d92b05d096d5b1660c15eb2ab47b27b
Callum Svadkovski <callum.svadkovski@hva.nl>
2023-10-04 14:33:24 +0200

    Embedded Device
    The rgb led is now controlled with the temperature sensor

--- Rgb_led/Rgb_led.ino
+++ Rgb_led/Rgb_led.ino
@@ -1,7 +1,15 @@
-int redpin = 16;   // select the pin for the red LED
-int bluepin = 12;  // select the pin for the blue LED
-int greenpin = 14; // select the pin for the green LED
-int temperature = 17;
+#include <DHT.h>
+
+#define DHTPIN 14
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+int redpin = 12;   // select the pin for the red LED
+int bluepin = 15;  // select the pin for the blue LED
+int greenpin = 13; // select the pin for the green LED
+
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
 
 void setup() {
   pinMode(redpin, OUTPUT);
@@ -11,12 +19,18 @@ void setup() {
 }
 
 void loop() {
-  if (temperature<18){
+  hum = dht.readHumidity();
+  temp= dht.readTemperature();
+
+  Serial.print(temp);
+  Serial.print(hum);
+
+  if (temp<18){
   analogWrite(redpin, 0);     // No red
   analogWrite(bluepin, 255);  // Full intensity blue, no green
   analogWrite(greenpin, 0);   // No green
   delay(2000); // Delay for 1 second (adjust as needed)
-  } else if (temperature >18 && temperature<25){
+  } else if (temp >18 && temp<25){
   analogWrite(redpin, 0);     // No red
   analogWrite(bluepin, 0);    // No blue
   analogWrite(greenpin, 255); // Full intensity green, no red or blue
