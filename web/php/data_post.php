2437981218fd04811b6f23d051b209f2b1f0c246
Callum Svadkovski <callum.svadkovski@hva.nl>
2023-10-05 13:02:18 +0200

    Web
    Fixed the php file to recieve data from the wemos and send to sql database

---
+++ web/php/data_post.php
@@ -0,0 +1,27 @@
+<?php
+
+// Make a connection to your database
+$conn = new mysqli("mariadb", "root", "7YKyE8R2AhKzswfN", "forcecast");
+
+if ($conn->connect_error) {
+  echo json_encode(array("success" => false, "error" => $conn->connect_error));
+  exit();
+}
+
+$data = json_decode(file_get_contents('php://input'));
+
+$temperature = $data->temperature;
+
+$humidity = $data->humidity;
+
+// Insert data into database using prepared statement
+$insertQuery = $conn->prepare("INSERT INTO `weather` (`temperature`, `humidity`) VALUES (?, ?)");
+
+$insertQuery->bind_param("dd", $temperature, $humidity);
+
+$insertQuery->execute();
+
+$insertQuery->close();
+
+$conn->close();
+?>
