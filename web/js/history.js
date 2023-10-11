dd499a530ce0b15726219113cc2bb7bb39de6f5c
Callum Svadkovski <callum.svadkovski@hva.nl>
2023-10-11 16:08:06 +0200

    Web
    Create dynamic weather card with data from weather api

--- web/js/history.js
+++ web/js/history.js
@@ -1,177 +1,123 @@
+const addButton = document.getElementById('addButton');
 
-// Array to store weather card data
-let weatherCards = [];
-
-// Function to create a weather card and add it to the array
-function createWeatherCard(location, temperature, humidity, weatherConditionCode) {
-    let cardClass;
-    if (temperature <= 10) {
-        cardClass = 'card-cold';
-    } else if (temperature >= 30) {
-        cardClass = 'card-hot';
-    } else {
-        cardClass = 'card-moderate';
+// Function to fetch weather data from the API
+async function fetchWeatherData(location) {
+    try {
+        const response = await fetch(`http://api.iot.hva-robots.nl/weather/${location}/compact`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        return null;
     }
+}
 
-    // Create an object to map weather condition codes to SVG icons
-    const weatherIcons = {
-        '116': '<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"><path d="M25 2C12.317 2 2 12.317 2 25s10.317 23 23 23 23-10.317 23-23S37.683 2 25 2zm0 43C13.868 45 3 34.132 3 23S13.868 3 25 3s22 10.868 22 20-10.868 22-22 22zm0-40C14.169 5 5 14.169 5 25s9.169 20 20 20 20-9.169 20-20S35.831 5 25 5zm-1 4.999v19h-2v-19h2zm1.001 28.001a2 2 0 1 1-.001-4.001 2 2 0 0 1 .001 4.001z"/></svg>',
-        // Add more weather condition codes and corresponding SVG icons as needed
-    };
+// Function to add a weather card to the frontend and local storage
+function addWeatherCard(location, temperature, humidity, observation_time, weatherDesc, precipMM) {
+    const weatherCards = document.getElementById('weather-cards');
+    const card = createWeatherCard(location, temperature, humidity, observation_time, weatherDesc, precipMM);
 
-    const weatherIconSvg = weatherIcons[weatherConditionCode] || ''; // Use an empty string if no icon found
+    weatherCards.appendChild(card);
+}
 
-    const cardHtml = `
-    <div class="col-md-10 mt-3">
-      <div class="card ${cardClass}">
+// Function to calculate background gradient class based on temperature
+function calculateBackgroundGradient(temperature) {
+    if (temperature < 0) {
+        return 'bg-primary bg-gradient bg-opacity-25'; // Very Cold (below 0°C)
+    } else if (temperature < 10) {
+        return 'bg-info bg-gradient bg-opacity-25'; // Cold (0°C to 10°C)
+    } else if (temperature < 20) {
+        return 'bg-success bg-gradient bg-opacity-25'; // Cool (10°C to 20°C)
+    } else if (temperature < 30) {
+        return 'bg-warning bg-gradient bg-opacity-25'; // Warm (20°C to 30°C)
+    } else if (temperature < 40) {
+        return 'bg-danger bg-gradient bg-opacity-25'; // Hot (30°C to 40°C)
+    } else {
+        return 'bg-dark bg-gradient bg-opacity-25'; // Very Hot (above 40°C)
+    }
+}
+
+
+// Function to create a weather card HTML element
+function createWeatherCard(location, temperature, humidity, observation_time, weatherDesc, precipMM) {
+    // Calculate the background color based on the temperature
+    const backgroundColor = calculateBackgroundGradient(temperature);
+
+    // Create the card element
+    const card = document.createElement('div');
+    card.className = 'weather-card';
+    card.innerHTML = `
+      <div class="card shadow-m col-10 mt-3 ${backgroundColor}">
+        <div class="card-header">
+          <h1>${location}</h1>
+        </div>
         <div class="card-body">
-          <h5 class="card-title">Weather in ${location}</h5>
-          <p class="card-text">Temperature: ${temperature}°C</p>
-          <p class="card-text">Humidity: ${humidity}%</p>
-          <div class="weather-icon">${weatherIconSvg}</div>
-          <button class="btn btn-danger remove-card" data-location="${location}">Remove</button>
+          <div class="row">
+            <div class="col-6">
+              <p>Temperature: ${temperature}*C</p>
+              <p>Humidity: ${humidity}%</p>
+              </div>
+            <div class="col-6 text-end">
+              <p>Conditions: ${weatherDesc}</p>
+              <p>Precipitation: ${precipMM}%</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-6">
+            <p style="font-weight:200">Last updated: ${observation_time}</p>
+            </div>
+            <div class="col-6 text-end">
+              <button class="btn btn-danger delete-button" data-location="${location}">Remove</button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  `;
+    `;
 
-    // Clear existing HTML in the container
-    const cardContainer = document.getElementById('weatherCardContainer');
-
-    // Add the card HTML to the container
-    cardContainer.innerHTML += cardHtml;
-
-    // Add the card data to the array
-    weatherCards.push({
-        location,
-        temperature,
-        humidity,
-    });
-
-    // Save the updated array to local storage
-    localStorage.setItem('weatherCards', JSON.stringify(weatherCards));
+    return card;
 }
 
-console.log("array:" + weatherCards);
-console.log("local" + localStorage.weatherCards);
+addButton.addEventListener('click', async () => {
+    const locationInput = document.getElementById('locationValue');
 
-function removeWeatherCard(location) {
-    console.log('Removing card for location:', location); // Debugging
-    // Find the index of the card with the specified location
-    const index = weatherCards.findIndex((card) => card.location === location);
-    console.log('Index:', index); // Debugging
+    const location = locationInput.value.trim();
 
-    if (index !== -1) {
-        // Remove the card from the array
-        weatherCards.splice(index, 1);
+    if (location) {
+        const weatherData = await fetchWeatherData(location);
 
-        // Update the HTML by removing the card with the matching location
-        const cardToRemove = document.querySelector(`[data-location="${location}"]`);
+        const data = {
+            location: location,
+            temperature: weatherData.data.FeelsLikeC,
+            humidity: weatherData.data.humidity,
+            observation_time: weatherData.data.observation_time,
+            weatherDesc: weatherData.data.weatherDesc[0].value,
+            precipMM: weatherData.data.precipMM
+        };
 
-
-
-        if (cardToRemove) {
-            localStorage.removeItem(location)
+        if (weatherData) {
+            fetch('/php/post_location.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            })
+            addWeatherCard(
+                location,
+                weatherData.data.FeelsLikeC,
+                weatherData.data.humidity,
+                weatherData.data.observation_time,
+                weatherData.data.weatherDesc[0].value,
+                weatherData.data.precipMM
+            )
+            locationInput.value = ''; // Clear the input field
         }
-
-        // Save the updated array to local storage
-        localStorage.setItem('weatherCards', JSON.stringify(weatherCards));
-    }
-}
-
-
-// Function to fetch weather data and create a card
-// Keep track of pending fetch requests
-const pendingFetchRequests = {};
-
-function fetchWeatherAndCreateCard(location) {
-    const apiUrl = `http://api.iot.hva-robots.nl/weather/${location}/compact`;
-
-    // Check if a fetch request is already pending for this location
-    if (pendingFetchRequests[location]) {
-        return; // If pending, do not make another request
-    }
-
-    // Set a flag to indicate a pending fetch request
-    pendingFetchRequests[location] = true;
-
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            // Once the request is complete, remove the flag
-            delete pendingFetchRequests[location];
-
-            const temperature = data.data.temp_C;
-            const humidity = data.data.humidity;
-            const weatherConditionCode = data.data.weatherCode;
-
-            // Check if the card for this location already exists
-            const existingCardIndex = weatherCards.findIndex((card) => card.location === location);
-
-            if (existingCardIndex === -1) {
-                // Create a weather card if it doesn't exist
-                createWeatherCard(location, temperature, humidity, weatherConditionCode);
-            } else {
-                alert("This location card already exists.");
-            }
-        })
-        .catch((error) => {
-            // Once the request is complete or if there's an error, remove the flag
-            delete pendingFetchRequests[location];
-
-            alert("Fill in a valid location");
-            console.error('Error fetching weather data:', error);
-        });
-}
-
-//localStorage.clear();
-//weatherCards = [];
-
-
-// Event listener for the "Add Location" button
-const addLocationButton = document.querySelector('.btn-success');
-
-addLocationButton.addEventListener('click', function () {
-    const locationInput = document.querySelector('.form-control');
-    const location = locationInput.value;
-
-    // Check if the location input is not empty
-    if (location.trim() !== '') {
-        fetchWeatherAndCreateCard(location);
-        locationInput.value = ''; // Clear the input field after adding
-    } else {
-        alert("Location can't be empty");
-    }
-
-    console.log(localStorage.weatherCards)
-});
-
-// Event listener for removing cards
-document.addEventListener('click', (e) => {
-    if (e.target.classList.contains('remove-card')) {
-        const location = e.target.getAttribute('data-location');
-        removeWeatherCard(location);
+        else {
+            console.log("No weather data found for" + location)
+        }
     }
 });
 
-// Load weather cards from local storage when the page is loaded
-window.addEventListener('load', function () {
-    const storedCards = localStorage.getItem('weatherCards');
-    if (storedCards) {
-        const storedWeatherCards = JSON.parse(storedCards);
-
-        // Iterate over stored cards and add only unique locations to the array
-        storedWeatherCards.forEach((storedCardData) => {
-            const existingCardIndex = weatherCards.findIndex((card) => card.location === storedCardData.location);
-
-            if (existingCardIndex === -1) {
-                weatherCards.push(storedCardData);
-            }
-        });
-
-        // Loop through the unique stored cards and create them on the page
-        weatherCards.forEach((cardData) => {
-            createWeatherCard(cardData.location, cardData.temperature, cardData.humidity);
-        });
-    }
-});
