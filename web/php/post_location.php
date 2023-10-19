decc430bdbe656e67ba2d941a3ca585ca124306c
Callum Svadkovski <callum.svadkovski@hva.nl>
2023-10-19 10:06:21 +0200

    Web: Post location data and delete location php

--- web/php/post_location.php
+++ web/php/post_location.php
@@ -13,25 +13,45 @@ $location = $jsonData->location;
 $temperature = $jsonData->temperature;
 $humidity = $jsonData->humidity;
 $time = $jsonData->observation_time;
-$description = $jsonData->weatherDesc;
+$conditions = $jsonData->weatherDesc;
 $precipitation = $jsonData->precipMM;
 
-// Check if the location already exists
-$checkQuery = $conn->prepare("SELECT COUNT(*) FROM `location` WHERE LOWER(`name`) = LOWER(?)");
-$checkQuery->bind_param("s", $location);
-$checkQuery->execute();
-$checkQuery->bind_result($count);
-$checkQuery->fetch();
-$checkQuery->close();
 
-if ($count == 0) {
-    // Location does not exist, so we can insert the data
-    $insertQuery = $conn->prepare("INSERT INTO `location` (`name`, `temperature`, `humidity`, `observation_time`, `conditions`, `precipitation`) VALUES (?, ?, ?, ?, ?, ?)");
-    $insertQuery->bind_param("siissd", $location, $temperature, $humidity, $time, $description, $precipitation);
-    $insertQuery->execute();
-    $insertQuery->close();
-} else {
-    echo "Location '$location' already exists.";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Check if the location already exists
+    $checkQuery = $conn->prepare("SELECT COUNT(*) FROM `location` WHERE LOWER(`name`) = LOWER(?)");
+    $checkQuery->bind_param("s", $location);
+    $checkQuery->execute();
+    $checkQuery->bind_result($count);
+    $checkQuery->fetch();
+    $checkQuery->close();
+
+    if ($count == 0) {
+        // Location does not exist, so we can insert the data
+        $insertQuery = $conn->prepare("INSERT INTO `location` (`name`, `temperature`, `humidity`, `observation_time`, `conditions`, `precipitation`) VALUES (?, ?, ?, ?, ?, ?)");
+        $insertQuery->bind_param("siissd", $location, $temperature, $humidity, $time, $conditions, $precipitation);
+        $insertQuery->execute();
+        $insertQuery->close();
+    } else {
+        return json_encode(array("success" => false, "error" => "$location already exists!"));
+    }
+}
+
+// Handle data deletion
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    $id = $jsonData->locationId;
+
+    $stmt = $conn->prepare("DELETE FROM `location` WHERE `locationId` = ?");
+
+    $stmt->bind_param("i", $id);
+
+    if ($stmt->execute()) {
+        json_encode("$stmt was executed successfully");
+    } else {
+        json_encode("$stmt did not execute successfully");
+    }
+
+    $stmt->close();
 }
 
 
