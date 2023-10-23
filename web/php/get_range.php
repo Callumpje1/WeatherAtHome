9bbe6d8101e7eb71512ba7fe718c7ad9eae1e12f
Callum Svadkovski <callum.svadkovski@hva.nl>
2023-10-23 17:11:06 +0200

    Web: Php file for sending temperature range

---
+++ web/php/get_range.php
@@ -0,0 +1,34 @@
+<?php
+
+$jsonData = json_decode(file_get_contents('php://input'));
+
+$minTemp = $jsonData->minTemp;
+$maxTemp = $jsonData->maxTemp;
+
+$data = [
+    'minTemp' => $minTemp,
+    'maxTemp' => $maxTemp,
+];
+
+$jsonData = json_encode($data);
+
+$wemosEndpoint = 'http://192.168.178.186/range';
+
+$options = [
+    'http' => [
+        'method' => 'POST',
+        'header' => 'Content-Type: application/json',
+        'content' => $jsonData,
+    ],
+];
+
+echo ($jsonData);
+
+$context = stream_context_create($options);
+$response = file_get_contents($wemosEndpoint, false, $context);
+
+if ($response === false) {
+    echo 'Error: Unable to send data to Wemos D1 Mini.';
+} else {
+    echo $response;
+}
