476d02f314b15f0ae71b0ec17c767eb92a4486d7
Callum Svadkovski <callum.svadkovski@hva.nl>
2023-10-23 17:10:41 +0200

    Web: Refactor index js for adding locations

--- web/js/index.js
+++ web/js/index.js
@@ -1,122 +1,192 @@
-const tempChart = document.getElementById('temperature-chart');
-const humidityChart = document.getElementById('humidity-chart');
-const homeTemp = document.getElementById('homeTemperature');
-const homeHumidity = document.getElementById('homeHumidity');
+const addButton = document.getElementById('addButton');
 
-function extractDayLabels(data) {
-    const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    return data.map(item => {
-        const dateStr = new Date(item.date);
-        const dayOfWeek = dateStr.getDay();
-        return daysOfWeek[dayOfWeek];
+fetchAllLocations();
+
+// Function to fetch weather data from the API
+async function fetchWeatherData(location) {
+    try {
+        const response = await fetch(`http://api.iot.hva-robots.nl/weather/${location}/compact`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        return null;
+    }
+}
+// Keep track of added locations
+const addedLocations = new Array();
+
+// Function to fetch weather data from the API
+async function fetchAllLocations() {
+    const weatherCards = document.getElementById('weather-cards'); // Get the container for weather cards
+
+    fetch('/php/get_location.php')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                const { locationId, name, temperature, humidity, observation_time, conditions, precipitation } = element;
+
+                if (!addedLocations.includes(name)) {
+                    // Create a weather card for each location retrieved from the database
+                    const card = createWeatherCard(locationId, name, temperature, humidity, observation_time, conditions, precipitation);
+
+                    // Add the weather card to the frontend
+                    weatherCards.appendChild(card);
+
+                    // Add the location to the set to mark it as added
+                    addedLocations.push(name);
+                }
+            });
+        })
+}
+
+
+// Function to add a weather card to the frontend
+function addWeatherCard(locationId, location, temperature, humidity, observation_time, weatherDesc, precipMM) {
+    const weatherCards = document.getElementById('weather-cards');
+
+    const card = createWeatherCard(locationId, location, temperature, humidity, observation_time, weatherDesc, precipMM);
+
+    weatherCards.appendChild(card);
+}
+
+// Function to calculate background gradient class based on temperature
+function addCardStyling(temperature) {
+    if (temperature < 0) {
+        return 'border-primary border-2'; // Very Cold (below 0°C)
+    } else if (temperature < 10) {
+        return 'border-info border-2'; // Cold (0°C to 10°C)
+    } else if (temperature < 20) {
+        return 'border-success border-2'; // Cool (10°C to 20°C)
+    } else if (temperature < 30) {
+        return 'border-warning border-2'; // Warm (20°C to 30°C)
+    } else if (temperature < 40) {
+        return 'border-danger border-2'; // Hot (30°C to 40°C)
+    } else {
+        return 'border-dark border-2'; // Very Hot (above 40°C)
+    }
+}
+
+function createWeatherCard(locationId, location, temperature, humidity, observation_time, weatherDesc, precipMM) {
+    const backgroundColor = addCardStyling(temperature);
+    // Create the card element
+    const card = document.createElement('div');
+    card.className = 'weather-card';
+    card.innerHTML = `
+          <div class="card shadow-sm col-10 mt-3 ${backgroundColor}">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-6">
+                  <h1>${temperature}&deg;C</h1>
+                  <h4>${location}</h4>
+                  <p>Humidity: ${humidity}%</p>
+                </div>
+                <div class="col-6 text-end">
+                  <p>Conditions: ${weatherDesc}</p>
+                  <p>Precipitation: ${precipMM} mm</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-6">
+                  <p style="font-weight:200">Last updated: ${observation_time}</p>
+                </div>
+                <div class="col-6 text-end">
+                  <button class="btn btn-danger delete-button" data-id="${locationId}">Remove</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
+    // Find the "Remove" button inside the card and attach a click event listener
+    const removeButton = card.querySelector('.delete-button');
+
+    removeButton.addEventListener('click', () => {
+        const locationId = removeButton.getAttribute('data-id');
+        removeWeatherCard(location, locationId);
     });
+
+    return card;
 }
 
-function extractAverageTemperatures(data) {
-    return data.map(item => item.averageTemperature);
-}
-
-function extractAverageHumidity(data) {
-    return data.map(item => item.averageHumidity);
-}
-
-function plotHistoricTemperatureChart() {
-    window.addEventListener('load', function () {
-        fetch('/php/get_average_climate.php')
-            .then(response => response.json())
-            .then(data => {
-                homeTemp.innerHTML = data[0].averageTemperature + "*C"
-                const dayLabels = extractDayLabels(data);
-                const averageTemperatures = extractAverageTemperatures(data);
-                new Chart(tempChart, {
-                    type: 'line',
-                    data: {
-                        labels: dayLabels,
-                        datasets: [{
-                            label: 'Temperature',
-                            lineTension: 0.3,
-                            data: averageTemperatures,
-                            borderColor: "#027d0d",
-                        }]
+function removeWeatherCard(location, locationId) {
+    if (confirm(`Are you sure you want to remove ${location}?`)) {
+        const deleteButton = document.querySelector(`.delete-button[data-id="${locationId}"]`);
+        if (deleteButton) {
+            const card = deleteButton.closest('.weather-card'); // Find the parent card element
+            if (card) {
+                // Make an AJAX request to delete the location from the database
+                fetch('php/post_location.php', {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
                     },
-                    options: {
-                        plugins: {
-                            legend: {
-                                display: false
-                            },
-                            title: {
-                                display: true,
-                            }
+                    body: JSON.stringify({ locationId: locationId }),
+                })
+                    .then(response => {
+                        if (response.ok) {
+                            card.remove();
+                            addedLocations.splice(addedLocations.indexOf(locationId), 1);
+                        } else {
+                            console.error(`Failed to remove location with ID ${locationId} from the database.`);
                         }
-                    }
-                });
-            })
-    });
+                    })
+                    .catch(error => {
+                        console.error('Error while making the AJAX request:', error);
+                    });
+            }
+        }
+    }
 }
 
-function plotHistoricHumidityChart() {
-    window.addEventListener('load', function () {
-        fetch('/php/get_average_climate.php')
-            .then(response => response.json())
-            .then(data => {
-                homeHumidity.innerHTML = data[0].averageHumidity + "%"
-                const dayLabels = extractDayLabels(data);
-                const averageHumidity = extractAverageHumidity(data);
-                new Chart(humidityChart, {
-                    type: 'line',
-                    data: {
-                        labels: dayLabels,
-                        datasets: [{
-                            label: 'Humidity',
-                            lineTension: 0.3,
-                            data: averageHumidity,
-                            borderColor: "#2832ed",
-                        }]
-                    },
-                    options: {
-                        plugins: {
-                            legend: {
-                                display: false
-                            },
-                            title: {
-                                display: true,
-                            }
-                        }
-                    }
-                });
+addButton.addEventListener('click', async () => {
+    const locationInput = document.getElementById('locationValue');
+
+    const location = locationInput.value.trim().toLowerCase();
+
+    if (addedLocations.includes(location)) {
+        alert("Location already exists.");
+    } else {
+        const weatherData = await fetchWeatherData(location);
+        const data = {
+            location: location,
+            temperature: weatherData.data.FeelsLikeC,
+            humidity: weatherData.data.humidity,
+            observation_time: weatherData.data.observation_time,
+            weatherDesc: weatherData.data.weatherDesc[0].value,
+            precipMM: weatherData.data.precipMM
+        };
+
+        fetch('/php/post_location.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    console.error(`Failed to add location ${location} to the database.`);
+                }
             })
-    });
-}
-
-const maxTempRange = document.getElementById('maxTempRange');
-const maxTempValue = document.getElementById('maxTempValue');
-
-maxTempRange.addEventListener("input", function () {
-    maxTempValue.textContent = maxTempRange.value;
-    console.log(maxTempRange.value)
-    fetch('/php/post_range.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ "maxTemp": maxTempRange.value }),
-    })
+            .then(responseData => {
+                if (responseData) {
+                    const locationId = responseData.locationId;
+                    addWeatherCard(locationId, location, weatherData.data.FeelsLikeC, weatherData.data.humidity, weatherData.data.observation_time, weatherData.data.weatherDesc[0].value, weatherData.data.precipMM);
+                    locationInput.value = '';
+                    addedLocations.push(location);
+                }
+            })
+            .catch(error => {
+                console.error('Error while adding location to the database:', error);
+            });
+    }
 });
 
-const minTempRange = document.getElementById('minTempRange');
-const minTempValue = document.getElementById('minTempValue');
 
-minTempRange.addEventListener("input", function () {
-    minTempValue.textContent = minTempRange.value;
-    console.log(minTempRange.value)
-    fetch('/php/post_range.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ "minTemp": minTempRange.value }),
-    })
-});
 
-plotHistoricHumidityChart();
-plotHistoricTemperatureChart();
+
