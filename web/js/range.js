4bf857d16b3d6ad2c6adf2ff9fdc120355a461a4
Callum Svadkovski <callum.svadkovski@hva.nl>
2023-10-23 17:10:23 +0200

    Web: Created temperature range with dual selector

---
+++ web/js/range.js
@@ -0,0 +1,39 @@
+var slider = document.getElementById('slider');
+var valueMin = document.getElementById('value-min');
+var valueMax = document.getElementById('value-max');
+
+noUiSlider.create(slider, {
+    start: [16, 25],
+    step: 0.1,
+    connect: [true, true, true],
+    range: {
+        'min': 0,
+        'max': 40
+    }
+});
+
+var connect = slider.querySelectorAll('.noUi-connect');
+
+var classes = ['c-1-color', 'c-2-color', 'c-3-color'];
+
+for (var i = 0; i < connect.length; i++) {
+    connect[i].classList.add(classes[i]);
+}
+
+slider.noUiSlider.on('update', function (values, handle) {
+    if (handle === 0) {
+        valueMin.innerHTML = Number(values[0]).toFixed(1); // Format to one decimal place
+    }
+    if (handle === 1) {
+        valueMax.innerHTML = Number(values[1]).toFixed(1); // Format to one decimal place
+    }
+
+    fetch('/php/get_range.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ "minTemp": values[0], "maxTemp": values[1] }),
+    });
+});
+
